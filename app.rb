486b5d315f20b7e263c17d6c0ed52095b660f8ca
OSSahota <osstechunlimited@gmail.com>
2018-12-11 15:10:37 +0000

    introducing params

--- app.rb
+++ app.rb
@@ -24,7 +24,14 @@ get '/notsosecret' do
   "Public message"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo","Oscar","Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
   erb(:index)
 end
